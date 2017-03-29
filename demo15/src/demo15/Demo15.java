ff44291e9aabfc1e5723aefd705b1e28df8a18e3
houlonglong <you@example.com>
2017-03-30 00:31:52 +0800

    面向对象构造方法

---
+++ demo15/src/demo15/Demo15.java
@@ -0,0 +1,11 @@
+package demo15;
+
+public class Demo15 {
+
+	public static void main(String[] args) {
+		 Person tom;
+		 tom = new Person(10);
+		 Person bob = new Person(1.8f);
+	}
+
+}
--- demo15/src/demo15/Person.java
+++ demo15/src/demo15/Person.java
@@ -1,5 +1,15 @@
 package demo15;
 
 public class Person {
-
+	int age;
+	float height;
+	Person(int age){
+		this.age = age;
+	}
+	Person(float height){
+		this.height = height;
+	}
+	Person(){
+		
+	}
 }
